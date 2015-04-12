fbe21724f8f77af7b8514a8a53b7e77132ba9608
Attila Györffy <attila@attilagyorffy.com>
2015-04-12 18:00:41 +0200

    Describe defaults

--- spec/mpd_spec.rb
+++ spec/mpd_spec.rb
@@ -4,4 +4,12 @@ describe MPD do
   it 'has a version number' do
     expect(MPD::VERSION).not_to be nil
   end
+
+  describe "defaults" do
+    it 'has sensible default settings' do
+      expect(subject.hostname).to eql('localhost')
+      expect(subject.port).to eql(6600)
+    end
+  end
+
 end
